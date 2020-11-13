51711aa5b2c8ef88f498f64480534fe50ceb3b20
harsh132 <prakashharsh32@gmail.com>
2020-11-14 00:40:47 +0530

    added oauth for google discord and github

--- server/server.js
+++ server/server.js
@@ -1,11 +1,44 @@
 require("dotenv/config");
-// require("./database");
 const express = require("express");
-// const cors = require("cors");
+const mongoose = require("mongoose");
+require("./db");
+const user = require('./models/userSchema');
+
+const cors = require("cors");
 const app = express();
 
-// app.use(cors());
+const g = require("./gauth");
+const d = require("./dauth");
+
+app.use(cors());
 app.use(express.json());
+
+app.post("/googleauth", async (req, res) => {
+    // access_token=req.body.access_token;
+    // refresh_token=req.body.refresh_token;
+    r = await g.verify({
+        access_token: req.body.gat,
+        refresh_token: req.body.grt,
+    });
+    console.log(r);
+    await user.find({gooogleEmail: r.email},(err,data)=>{
+        if(!data){
+            
+        }
+    })
+    res.send(r);
+});
+
+app.get("/dauthurl",(req,res) => {
+    res.redirect(d.discordAuthUrl('harsh'));
+})
+
+app.get("/discordauth", (req,res) => {
+    userinfo =  d.discordAuth(req.body.state, req.body.code);
+    console.log(req.body.state, req.body.code);
+    res.header("content-type: application/json")
+    res.send(JSON.stringify(userinfo));
+})
 
 // Firing up the server
 const port = process.env.PORT || 5000;
